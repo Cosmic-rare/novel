a1f21963cf541b7d1d27b0b3e996b5481f7041ca
Cosmic-rare <77323801+Cosmic-rare@users.noreply.github.com>
2021-09-08 21:30:08 +0900

    ページ数の判定を簡略化

--- func.py
+++ func.py
@@ -15,20 +15,11 @@ def check_url(url):
     else:
         return True
 
-def isint(s):  # 整数値を表しているかどうかを判定
-    try:
-        int(s)  # 文字列を実際にint関数で変換してみる
-    except ValueError:
-        return False
-    else:
-        return True
-
 def get_part(ncode):
     while True:
         part = input('Part > ')
-        if isint(part):
-            url = 'https://ncode.syosetu.com/{}/{:d}'.format(ncode, int(part))
-            if check_url(url):
-                break
+        url = 'https://ncode.syosetu.com/{}/{}'.format(ncode, part)
+        if check_url(url):
+            break
     
     return int(part)
